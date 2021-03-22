d2e2f51e762c8db4b07eee1773fea2d8ddd8e561
Jan Kück <jkueck@cargosoft.de>
2021-03-22 13:25:47 +0100

    add constructor with log info

--- src/main/java/de/jkueck/Main.java
+++ src/main/java/de/jkueck/Main.java
@@ -2,7 +2,12 @@ package de.jkueck;
 
 public class Main {
 
+    public Main() {
+        System.out.println("call constructor");
+    }
+
     public static void main(String[] args) {
         System.out.println("starting application...");
     }
+
 }
